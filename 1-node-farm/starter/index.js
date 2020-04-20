17884f9e9d6505e959be542a813e6e18329a6155
Gagan <gaganjot.singh.randhawa@sap.com>
2020-04-20 23:45:46 +0530

    initial

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,9 +1,23 @@
 const fs = require('fs');
+const http = require('http');
+///////////////////////////
 
-const textin = fs.readFileSync('./txt/input.txt', 'utf-8');
+// const textin = fs.readFileSync('./txt/input.txt', 'utf-8');
 
-console.log(textin)
+// console.log(textin)
 
-const textout = `This is what we know about avocado ${textin} Createed on ${Date.now()}`;
+// const textout = `This is what we know about avocado ${textin} Createed on ${Date.now()}`;
 
-fs.writeFileSync('.txt/output.txt', textout);
+// fs.writeFileSync('./txt/output.txt', textout);
+
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+//   console.log(data)
+// })
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from httpserver')
+})
+
+server.listen(9999, 'localhost', () => {
+  console.log('server running')
+})
